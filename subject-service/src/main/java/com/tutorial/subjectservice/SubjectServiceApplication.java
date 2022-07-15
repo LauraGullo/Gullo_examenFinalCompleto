8338e11db19ce48db1524b9b461802db5357164b
LauraGullo <marialgullo@gmail.com>
2022-07-15 18:32:42 -0300

    agrega subject

--- subject-service/src/main/java/com/tutorial/subjectservice/SubjectServiceApplication.java
+++ subject-service/src/main/java/com/tutorial/subjectservice/SubjectServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CarServiceApplication {
+public class SubjectServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarServiceApplication.class, args);
+		SpringApplication.run(SubjectServiceApplication.class, args);
 	}
 
 }
